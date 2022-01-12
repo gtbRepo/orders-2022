b9b345ad036766466a08c96a2380923a169604c6
gtbRepo <grzegorz004-1993@o2.pl>
2022-01-12 12:29:37 +0100

    zadanie

--- src/test/java/pl/edu/wszib/order/application/product/ProductSamples.java
+++ src/test/java/pl/edu/wszib/order/application/product/ProductSamples.java
@@ -3,6 +3,7 @@ package pl.edu.wszib.order.application.product;
 import java.math.BigDecimal;
 
 public class ProductSamples {
+    //TODO DOADAĆ WIĘCEJ PRODUKTÓW
     public static final Product CHOCOLATE = new Product(ProductId.create(),
             "Czekolada", BigDecimal.valueOf(4));
 }
