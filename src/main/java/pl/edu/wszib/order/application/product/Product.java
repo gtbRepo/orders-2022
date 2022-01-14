4c118e900092fa9a8dbfd3c385ce4bd81ee14963
gtbRepo <grzegorz004-1993@o2.pl>
2022-01-14 17:49:14 +0100

    product validation annotations

--- src/main/java/pl/edu/wszib/order/application/product/Product.java
+++ src/main/java/pl/edu/wszib/order/application/product/Product.java
@@ -2,14 +2,22 @@ package pl.edu.wszib.order.application.product;
 
 import java.math.BigDecimal;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.Value;
 
 @Value
 public class Product {
+    @NotNull
     private final ProductId id;
+    @NotNull
+    @Min(2)
+    @Max(50)
     private final String name;
     // można docelowo przejść na:
     // https://github.com/JavaMoney/jsr354-api
+    @NotNull
     private final BigDecimal price;
 
     public boolean hasId(final String productId) {
