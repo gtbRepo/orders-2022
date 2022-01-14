710df28c5de2919b0ec35d2026f25ee717ba5786
gtbRepo <grzegorz004-1993@o2.pl>
2022-01-15 00:30:15 +0100

    split modules

---
+++ orders-application/src/main/java/pl/edu/wszib/order/application/order/OrderItem.java
@@ -0,0 +1,44 @@
+package pl.edu.wszib.order.application.order;
+
+import pl.edu.wszib.order.api.OrderItemApi;
+import pl.edu.wszib.order.application.product.Product;
+
+import java.math.BigDecimal;
+
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@ToString
+public class OrderItem {
+    private final Product product;
+    private final Integer quantity;
+    private final BigDecimal amount;
+
+    public OrderItem(final Product product,
+                     final Integer quantity) {
+        this.product = product;
+        this.quantity = quantity;
+        this.amount = calculateAmount();
+    }
+
+    private BigDecimal calculateAmount() {
+        return BigDecimal.valueOf(0);
+    }
+
+    public static OrderItem create(final Product product,
+                                   final Integer quantity) {
+        return new OrderItem(product, quantity);
+    }
+
+    public OrderItemApi toApi() {
+        return new OrderItemApi(product.getId().asBasicType(),
+                product.getName(),
+                product.getPrice(), quantity, amount);
+
+    }
+
+    public boolean hasProductId(final String productId) {
+        return product.hasId(productId);
+    }
+}
