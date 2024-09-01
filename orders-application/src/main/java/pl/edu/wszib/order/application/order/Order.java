122fa83498d003bd7cc4145fe73092ab8b3802d9
Grzegorz Burdek <grzegorz004-1993@o2.pl>
2024-09-01 15:16:51 +0200

    Repetition: split modules

---
+++ orders-application/src/main/java/pl/edu/wszib/order/application/order/Order.java
@@ -0,0 +1,40 @@
+package pl.edu.wszib.order.application.order;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+import pl.edu.wszib.order.api.OrderApi;
+import pl.edu.wszib.order.api.OrderState;
+
+import java.math.BigDecimal;
+
+@AllArgsConstructor
+@ToString
+public class Order {
+    @Getter
+    private final OrderId id;
+    private final OrderState state;
+    private final OrderItems items;
+    private final BigDecimal amount;
+
+    public static Order create() {
+        return new Order(
+                OrderId.create(),
+                OrderState.CREATED,
+                OrderItems.create(),
+                BigDecimal.ZERO //TODO Impl
+        );
+    }
+
+    public Order addItem(final OrderItem orderItem) {
+        return new Order(id, state, items.add(orderItem), amount); //TODO Impl
+    }
+
+    public OrderApi toApi() {
+        return new OrderApi(id.asBasicType(), state, items.toApi(), amount);
+    }
+
+    public Order removeItem(String productId) {
+        return new Order(id, state, items.remove(productId), amount);
+    }
+}
